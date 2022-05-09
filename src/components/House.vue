377f220d8f57f8ab755da304c36d3de8980248ce
Dmitry <Dmitrytbp@gmail.com>
2022-05-09 18:20:31 +0300

    Доработал логику отображения домов

--- src/components/House.vue
+++ src/components/House.vue
@@ -1,33 +1,55 @@
 <template>
-  <v-col cols="4">
-    <div class="house">
-      <div
-        v-for="section of houseData"
-        :key="section.id"
-        class="house__section"
-      >
-        <Flats :section="section" />
-        <h3 class="house__section-title">{{ section.title }}</h3>
-      </div>
+  <div class="house">
+    <div
+      v-for="(section, index) of housesData"
+      :key="section.id"
+      class="house__section"
+    >
+      <Flats :section="section" :section-index="index" />
+      <h3 class="house__section-title">{{ section.title }}</h3>
     </div>
-  </v-col>
+  </div>
 </template>
 
 <script>
 import Flats from './Flats.vue';
+
 export default {
   props: {
-    houseData: { type: Object, require: true },
-  },
-  created() {
-    console.log('houseData', this.houseData);
+    housesData: { type: Object, require: true },
   },
   components: { Flats },
+
+  created() {
+    console.log('housesData', this.housesData);
+  },
+
+  methods: {},
 };
 </script>
 
 <style lang="scss" scoped>
+@import '../styles/variables';
 .house {
+  display: flex;
+  align-items: flex-end;
   height: 100%;
+  overflow-y: scroll;
+  will-change: scroll-position;
+
+  &__section {
+    margin-right: 30px;
+
+    &:first-child {
+      & .house__section-title {
+        margin-left: calc($flat-size + 10px);
+      }
+    }
+  }
+
+  &__section-title {
+    font-weight: 400;
+    color: $grey;
+  }
 }
 </style>
