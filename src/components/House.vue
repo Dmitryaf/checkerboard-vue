f6ecea3cb5d1fb280b840cd4354d4f020f8b3789
Dmitry <Dmitrytbp@gmail.com>
2022-05-09 12:33:32 +0300

    Компонент для отображения дома

---
+++ src/components/House.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-col cols="4">
+    <div class="house">
+      <div
+        v-for="section of houseData"
+        :key="section.id"
+        class="house__section"
+      >
+        <Flats :section="section" />
+        <h3 class="house__section-title">{{ section.title }}</h3>
+      </div>
+    </div>
+  </v-col>
+</template>
+
+<script>
+import Flats from './Flats.vue';
+export default {
+  props: {
+    houseData: { type: Object, require: true },
+  },
+  created() {
+    console.log('houseData', this.houseData);
+  },
+  components: { Flats },
+};
+</script>
+
+<style lang="scss" scoped>
+.house {
+  height: 100%;
+}
+</style>
